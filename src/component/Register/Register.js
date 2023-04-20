ddda7d0f574afadc6aaa936b232037820c17d460
welfredk <welfredkng@gmail.com>
2023-04-20 08:37:32 +0800

    updating link

--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
     }
 
     onSubmitRegister = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://smartbrainserver-fhye.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
